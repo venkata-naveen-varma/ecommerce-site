77c4c0bdcac1d3c48876e2a92b25c927fd3e3539
venkata-naveen-varma <venkata2naveen@gmail.com>
2024-02-01 21:40:50 -0500

    user stages, Shop categories

---
+++ frontend/src/pages/Product.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import { useParams } from 'react-router-dom';
+import BreadCrum from "../components/BreadCrums/BreadCrum";
+import { ShopContext } from "../context/ShopContext";
+import ProductDisplay from "../components/ProductDisplay/ProductDisplay";
+
+const Product = () => {
+    const {all_product} = useContext(ShopContext);
+    const {productId} = useParams();
+    const product = all_product.find((e)=> e.id === Number(productId));
+    return (
+        <div>
+            <BreadCrum product={product}/>
+            <ProductDisplay product={product}/>
+        </div>
+    )
+}
+
+export default Product
