d657dee172b62353688e3040010998c20ed113ae
venkata-naveen-varma <venkata2naveen@gmail.com>
2024-02-15 12:27:14 -0500

    Cart page

--- frontend/src/components/CartItems/CartItems.jsx
+++ frontend/src/components/CartItems/CartItems.jsx
@@ -19,13 +19,13 @@ const CartItems = () => {
         {all_product.map((e)=>{
             if(cartItems[e.id]>0){
                 return (<div>
-                            <div className="cartitems-format">
+                            <div className="cartitems-format cartitems-format-main">
                                 <img src={e.image} alt="" className='carticon-product-icon'/>
                                 <p>{e.name}</p>
                                 <p>${e.new_price}</p>
                                 <button className='cartitems-quantity'>{cartItems[e.id]}</button>
-                                <p>{e.new_price*cartItems[e.id]}</p>
-                                <img src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
+                                <p>${e.new_price*cartItems[e.id]}</p>
+                                <img className='cartitems-remove-icon' src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
                             </div>
                             <hr />
                         </div>) 
