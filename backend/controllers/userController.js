734a3c4ff123854237b3fee580b0e647740c1826
venkata-naveen-varma <venkata2naveen@gmail.com>
2024-02-22 20:18:21 -0500

    init

---
+++ backend/controllers/userController.js
@@ -0,0 +1,7 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import User from "../models/users.js";
+
+export const registerUser = async (req, res)=>{
+    
+}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
-// import {
-//     registerUser, loginUser, getUser, logoutUser, getUserDetails
-// } from "../controllers/users.js";
+import {
+    registerUser
+} from "../controllers/users.js";
 // import Auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 // router.get("/", Auth, getUser);
-// router.post("/register", registerUser);
+router.post("/register", registerUser);
 // router.post("/login", loginUser);
 // router.post("/logout", logoutUser)
 // router.post("/details", getUserDetails);
