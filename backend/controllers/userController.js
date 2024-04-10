cb2dabb27fd3c1d37fd3e5a0c2e31b534830ca08
Venkata Naveen Varma Vegesna <115595566+venkata-naveen-varma@users.noreply.github.com>
2024-04-09 20:09:18 -0400

    Delete backend directory

--- backend/controllers/userController.js
+++
@@ -1,7 +0,0 @@
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import User from "../models/users.js";
-
-export const registerUser = async (req, res)=>{
-    
-}
--- backend/utils.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export async function connectToDB(){
-    try{
-        await mongoose.connect(process.env.MONGODB_CONNECTION);
-    }catch(err){
-        console.log(`Error while connecting to db - ${err}`);
-    }
-}
